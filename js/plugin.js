e44d849692b043ae2b323dd9e9b2a47f45949004
emmattfu <makcimdjan@gmail.com>
2018-06-22 14:28:05 +0300

    finished

--- js/plugin.js
+++ js/plugin.js
@@ -16,80 +16,25 @@ class Http {
 
         this.http.send();
     }
-
-    post(url, data, callback) {
-        if (!data) return callback('Error: Data обязательный параметр');
-
-        this.http.open("POST", url);
-        this.http.setRequestHeader("Content-type", "application/json");
-
-        const self = this; // сохранить контекст вызова
-        this.http.addEventListener("load", function () {
-            if (self.http.status === 200 || self.http.status === 201) {
-                callback(null, self.http.responseText);
-            } else {
-                callback(`Error: ${self.http.status}`, null);
-            }
-        });
-
-        this.http.send(JSON.stringify(data));
-    }
-
-    put(url, data, callback) {
-        if (!data) return callback('Error: Data обязательный параметр');
-
-        this.http.open("PUT", url);
-        this.http.setRequestHeader("Content-type", "application/json");
-
-        const self = this; // сохранить контекст вызова
-        this.http.addEventListener("load", function () {
-            if (self.http.status === 200 || self.http.status === 201) {
-                callback(null, self.http.responseText);
-            } else {
-                callback(`Error: ${self.http.status}`, null);
-            }
-        });
-
-        this.http.send(JSON.stringify(data));
-    }
-
-    delete(url, callback) {
-        this.http.open("DELETE", url);
-        this.http.setRequestHeader("Content-type", "application/json");
-
-        const self = this; // сохранить контекст вызова
-        this.http.addEventListener("load", function () {
-            if (self.http.status === 200 || self.http.status === 201) {
-                callback(null, "Post deleted");
-            } else {
-                callback(`Error: ${self.http.status}`, null);
-            }
-        });
-
-        this.http.send();
-    }
 }
 
 const http = new Http();
 // Получаем кнопку
 const btn = document.querySelector('button');
+btn.disabled = false;
 // Получаем список
 const ul = document.getElementById('user-list');
-// Получаем дочерние элементы списка (HTMLCollection)
-const li = ul.children;
-const liArr = Array.from(li);
-
 
 // Вешаем событие клик на кнопку
 btn.addEventListener('click', function (e) {
     http.get('https://jsonplaceholder.typicode.com/users', function (err, res) {
         // переводим полученые данные в массив
         let userList = (JSON.parse(res));
-        console.log(userList);
-        // перебираем полученый массив и выводим в разметку имя каждого юзера
+        // перебираем полученый массив и выводим в разметку имя каждого юзера и блокируем кнопку вывода пользователей
         userList.forEach(function (user) {
-            let userName = `<li class="user-name">${user.name}</li>`;
+            let userName = `<li class="user-name text-white">${user.name}</li>`;
             ul.insertAdjacentHTML('beforeend', userName);
+            btn.disabled = true;
         })
     });
 });
@@ -102,17 +47,34 @@ ul.addEventListener('click', function (e) {
             let userList = (JSON.parse(res));
             userList.forEach(function (user) {
                 if (user.name === e.target.textContent) {
+                    // получаем карточку с информацией о пользователе
                     const currenttDiv = document.querySelector('.user-info');
+                    // если карточка уже существует то удаляем ее
                     if (currenttDiv) {
                         currenttDiv.remove();
                     }
-                    let info = `<div class="user-info"><span>name: ${user.name} <button class="close-info"><i class="fas fa-times"></i></button><br>phone: ${user.phone}<br>company: ${user.company.name}</span></div>`;
+                    // создаем разметку карточки с информацией о пользователе
+                    let info = `<div class="user-info text-white">
+                                    <span class="user-info-name">name: ${user.name}</span>
+                                    <span class="user-info-phone">phone: ${user.phone}</span>
+                                    <span class="user-info-company">company: ${user.company.name}</span>
+                                    <button class="hide-info text-red"><i class="fas fa-times"></i></button>
+                                </div>`;
+                    // выводим в разметку карточку пользователя
                     e.target.insertAdjacentHTML('afterend', info);
                 }
             })
         });
 
     }
+    if (e.target.classList.contains('fa-times')) {
+        // находим родителя кнопки удаления и удаляем его
+        const currnetDiv = e.target.closest('div');
+        currnetDiv.remove();
+    }
 });
+
+
+
 
 
